4a47d8aae54f2df53c8992dacc012c433857ea25
KIN_002 <67863453+LiKING002@users.noreply.github.com>
2021-11-04 21:38:01 +0800

    Add files via upload

---
+++ X_01/X_01/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace X_01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int math1 = 1; math1 <= 9; math1++) 
+            {
+                for (int math2 = 1; math2 <= 9; math2++) 
+                {
+                    textBox1.AppendText(text: math1 + "*" + math2 + "=" + (math1 * math2).ToString("00") + "\t");
+                }
+            textBox1.AppendText("\r\n");
+            }
+        }
+
+        
+    }
+}
---
+++ X_2 (1)/X-2/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace X_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int math1, math2;
+            for (math1 = 2; math1 < 200; math1++)
+            {
+                for (math2 = 2; math2< math1; math2++)
+                {
+                    if (math1 % math2 == 0 && math1 != math2)
+                    {
+                        break;            
+                    }
+                }
+                if (math1 % math2 == 0 && math1 == math2) 
+                {
+                    textBox1.AppendText(text: math1 + "\t");
+                }
+            }
+        }
+    }
+}
